560e7396936be1c99af9a6cb87351fe2e44580d4
morax <momorax4023@gmail.com>
2025-11-22 00:12:36 +0100

    Mobile Chat UI Polish again

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
       {/* The Navbar MUST be here */}
       <Navbar />
       
-      <main className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <main className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 mt-16">
         {children}
       </main>
     </div>
