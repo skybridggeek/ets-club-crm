3ed281bce3ddca288975237b11f44195c02e5a8f
skybridggeek <elmahdikaloune@gmail.com>
2026-03-13 00:19:50 +0100

    fix: TypeScript type error in pushNotifications

--- lib/pushNotifications.ts
+++ lib/pushNotifications.ts
@@ -58,7 +58,7 @@ export async function subscribeUserToPush(userId: number): Promise<boolean> {
 
     const subscription = await reg.pushManager.subscribe({
       userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+      applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY).buffer as ArrayBuffer,
     });
 
     const subJSON = subscription.toJSON();
